e85a31b24a5d64168bb6ea8cace6f29752fa9495
mozart reina <mozart@mozartreina.com>
2015-01-06 18:57:15 +0900

    Added prorating functionality to calculate pass revenue per month.

--- shala-sys-tests.lisp
+++ shala-sys-tests.lisp
@@ -15,16 +15,16 @@
 (defun populate-dummy-passes ()
   (progn 
     (new-pass (student-from-name "yukako kotani") (make-pass :type 'm
-                                                             :start-date (local->unix (add-months -2 (time-now)))
+                                                             :start-date (local->unix (adjust-months (time-now) -2))
                                                              :amt 17000)) ;; Has one pass that was bought 2 months ago (1 month type).
     (new-pass (student-from-name "kaori kawai") (make-pass :type 'w
-                                                           :start-date (local->unix (add-days -6 (time-now)))
+                                                           :start-date (local->unix (adjust-days (time-now) 6))
                                                            :amt 6000)) ;; Has one pass that was bought 6 days ago (1 week type).
     (new-pass (student-from-name "yoko kamioka") (make-pass :type 'm
                                                             :start-date (local->unix (time-now))
                                                             :amt 17000)) ;; Has one pass that was bought now (1 month type)
     (new-pass (student-from-name "taeko fujimoto") (make-pass :type 'm
-                                                              :start-date (local->unix (add-days -45 (time-now)))
+                                                              :start-date (local->unix (adjust-days (time-now) -45))
                                                               :amt 17000)))) ;; Has one pass that was bought 45 days ago (1 month type)
 
 (defun last-month ()
--- time.lisp
+++ time.lisp
@@ -89,3 +89,59 @@
 ;;               (make-drop-in :date (unix->local (elt drop-in 1))
 ;;                             :amt (elt drop-in 3)))
 ;;           drop-in-list))
+(defun adjust-days (timestamp days)
+  (local-time:adjust-timestamp timestamp (offset :day days)))
+
+(defun adjust-months (timestamp months)
+  "Add or subtract months from the timestamp, returns a new timestamp"
+  (local-time:adjust-timestamp timestamp (offset :month months)))
+
+(defun get-year (timestamp)
+  "Extract the year from the timestamp"
+  (local-time:timestamp-year timestamp))
+
+(defun get-month (timestamp)
+  "Extract the month from the timestamp"
+  (local-time:timestamp-month timestamp))
+
+(defun get-pass-on (year month pass-list)
+  "Returns the pass of the month"
+  (first (remove-if-not #'(lambda (pass)
+                                     (equalp (cons (local-time:timestamp-year (get-start-date pass))
+                                                   (local-time:timestamp-month (get-start-date pass)))
+                                             (cons year month)))
+                                 pass-list)))
+
+(defun prorate-till-month-end (pass)
+  "Calculates the prorated amount of the pass based on purchase date and the number of days in the month. Returns as a second value the difference (carry-over for next month) between the total amount paid and the prorated amount."
+  (let* ((month-days (local-time:days-in-month (local-time:timestamp-month (get-start-date pass))
+                                              (local-time:timestamp-year (get-start-date pass))))
+         (start-day (local-time:timestamp-day (get-start-date pass)))
+         (prorated-amt (* (/ (float (getf pass :amt)) month-days)
+                          (- month-days start-day))))
+    (values prorated-amt
+            (- (getf pass :amt)
+               prorated-amt))))
+
+(defun carry-over+current (pass)
+  "Add the previous month's pass's carry-over to the current month's prorated amount"
+  (let ((prev-month (get-pass-on (get-year (adjust-months (get-start-date pass) -1))
+                                 (get-month (adjust-months (get-start-date pass) -1))))
+        (current-month pass))
+    (if (and prev-month current-month)
+        (+ (prorate-till-month-end current-month)
+           (nth-value 1 (prorate-till-month-end prev-month)))
+        (nth-value 0 (prorate-till-month-end current-month)))))
+
+(defun total-for (year month)
+  "Retrieve the pass of the year and month given and return the carry-over + prorated amount if applicable, or just the carry-over amount from the previous pass (if existent) if there is no pass for the the given year and month."
+  (let* ((timestamp (local-time:encode-timestamp 1 1 1 1 1 month year))
+         (pass (if (get-pass-on year month)
+                   (get-pass-on year month)
+                   (get-pass-on (get-year (adjust-months timestamp -1))      ;No pass for the given year and month, try extracting the previous month's pass
+                                (get-month (adjust-months timestamp -1))))))
+    (when pass
+      (if (equalp (get-month (get-start-date pass))                          ;Pass for given year and month exists
+                  month)
+          (carry-over+current pass)
+          (nth-value 1 (prorate-till-month-end pass)))))) 
