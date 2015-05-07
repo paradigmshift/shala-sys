130c2eaba9a6423314a30da0dcda07b6291abcba
mozart reina <mozart@mozartreina.com>
2015-05-08 03:34:05 +0900

    DONE: remove-expense method. TODO: find and edit expense method

--- db-funcs.lisp
+++ db-funcs.lisp
@@ -8,10 +8,13 @@
 
 (defparameter *expense-list* "expenses")
 
-(defmacro define-method-with-update (method-name (&rest args) &body body)
+;;------------------------------------------------------------------------------
+;; Student functions
+;;------------------------------------------------------------------------------
+(defmacro defstudent-method-with-update (method-name (&rest args) &body body)
   "Defines a method and an :after method for persisting changes by the method to the database"
   (let ((student (first args)))
-    `(progn 
+    `(progn
        (defmethod ,method-name ,args
          ,@body)
        (defmethod ,method-name :after ,args
@@ -19,31 +22,31 @@
                     (db.update *student-list* ($ "name" (name ,student))
                                student-doc))))))
 
-(define-method-with-update new-drop-in (student drop-in)
+(defstudent-method-with-update new-drop-in (student drop-in)
   (push drop-in (drop-in student)))
 
-(define-method-with-update new-pass (student pass)
+(defstudent-method-with-update new-pass (student pass)
   (push pass (pass student))
   (setf (pass student)
         (sort-pass-dates (pass student))))
 
-(define-method-with-update remove-pass (student pass)
+(defstudent-method-with-update remove-pass (student pass)
   (setf (pass student)
         (sort-pass-dates (remove (first (pass-list-dates->yy-mm-dd (list pass)))
                                  (pass-list-dates->yy-mm-dd (pass student)) :test #'equalp))))
 
-(define-method-with-update remove-drop-in (student drop-in)
+(defstudent-method-with-update remove-drop-in (student drop-in)
   (setf (drop-in student)
         (sort-pass-dates (remove (first (pass-list-dates->yy-mm-dd (list drop-in)))
                                  (pass-list-dates->yy-mm-dd (drop-in student)) :test #'equalp))))
 
-(define-method-with-update remove-last-pass (student)
+(defstudent-method-with-update remove-last-pass (student)
   (setf (pass student) (rest (pass student))))
 
 ;;"Searches pass-list for pass and inserts a new pass in place of that with fields modified according to arg-list"
-(define-method-with-update find-and-edit-pass (student pass pass-list new-pass)
+(defstudent-method-with-update find-and-edit-pass (student pass pass-list new-pass)
   (multiple-value-bind (converted-pass converted-pass-list) (pass-and-pass-list-dates->yy-mm-dd pass pass-list)
-    (when (member converted-pass converted-pass-list :test #'equalp) 
+    (when (member converted-pass converted-pass-list :test #'equalp)
       (progn
         (setf (elt pass-list (position converted-pass converted-pass-list :test #'equalp))
               new-pass)
@@ -51,15 +54,24 @@
               ;; Sorts the pass dates non destructively then converts the date fields to local-time
               (pass-list-dates->timestamp (sort-pass-dates pass-list)))))))
 
-(define-method-with-update find-and-edit-drop-in (student pass pass-list new-pass)
+(defstudent-method-with-update find-and-edit-drop-in (student pass pass-list new-pass)
   (multiple-value-bind (converted-pass converted-pass-list) (pass-and-pass-list-dates->yy-mm-dd pass pass-list)
-    (when (member converted-pass converted-pass-list :test #'equalp) 
+    (when (member converted-pass converted-pass-list :test #'equalp)
       (progn
         (setf (elt pass-list (position converted-pass converted-pass-list :test #'equalp))
               new-pass)
         (setf (drop-in student)
               ;; Sorts the pass dates non destructively then converts the date fields to local-time
               (pass-list-dates->timestamp (sort-pass-dates pass-list)))))))
+
+;;------------------------------------------------------------------------------
+;; Expense functions
+;;------------------------------------------------------------------------------
+
+(defmethod remove-expense (expense)
+  "Remove EXPENSE from the DB"
+  (db.delete *expense-list* ($ ($ "comment" (comment expense))
+                               ($ "date" (local->unix (date expense))))))
 
 (defun pass-and-pass-list-dates->yy-mm-dd (pass pass-list)
   "Converts the date fields of pass and pass-list to a string in yy-mm-dd format"
